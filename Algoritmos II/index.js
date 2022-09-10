8ec61d9f188a68fb1cd4d41522350ec55fc9e6d9
Mateus Orlandin Dias <mateus.dias@meta.com.br>
2022-09-10 19:10:59 -0300

    Aula 1 algoritmos II

---
+++ Algoritmos II/index.js
@@ -0,0 +1,37 @@
+//algoritmo que percorre dois arrays diferentes e ordena em um só array em ordem de preço
+const { edFolha, edGalho } = require('./arrays');
+
+function juntaListas(Lista1, Lista2) {
+    let listaFinal = [];
+    let posicaoAtualLista1 = 0;
+    let posicaoAtualLista2 = 0;
+    let atual = 0;
+
+    while (posicaoAtualLista1 < Lista1.length && posicaoAtualLista2 < Lista2.length) {
+        if (Lista1[posicaoAtualLista1].preco < Lista2[posicaoAtualLista2].preco) {
+            listaFinal[atual] = Lista1[posicaoAtualLista1];
+            posicaoAtualLista1++;
+        } else {
+            listaFinal[atual] = Lista2[posicaoAtualLista2];
+            posicaoAtualLista2++;
+        }
+
+        atual++;
+    }
+
+    while (posicaoAtualLista1 < Lista1.length) {
+        listaFinal[atual] = Lista1[posicaoAtualLista1];
+        posicaoAtualLista1++;
+        atual++;
+    }
+
+    while (posicaoAtualLista2 < Lista2.length) {
+        listaFinal[atual] = Lista2[posicaoAtualLista2];
+        posicaoAtualLista2++;
+        atual++;
+    }
+
+    return listaFinal;
+}
+
+console.log(juntaListas(edFolha, edGalho));
