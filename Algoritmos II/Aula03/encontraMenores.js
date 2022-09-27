0bce6ed1a16fcc5cd4c9b8c44dad5198a7aaec5f
Mateus Orlandin Dias <mateus.dias@meta.com.br>
2022-09-26 23:34:45 -0300

    2/3 Quick Sort - Aula03

---
+++ Algoritmos II/Aula03/encontraMenores.js
@@ -0,0 +1,48 @@
+/*
+Quick Sort
+
+Primeiro passo - Posicionar o pivo
+Segundo passo - Passar os elementos menores que o pivô para a sua esqueda
+Terceiro passo - 
+*/
+const array = require('./arrays');
+
+function encontraMenores (pivo, array){
+    let menores = 0;
+
+    for(let atual = 0; atual < array.length; atual++){
+        let produtoAtual = array[atual];
+        if(produtoAtual.preco < pivo.preco){
+            menores++;
+        }
+    }
+
+    trocaLugar(array, array.indexOf(pivo), menores);
+    return array;
+}
+
+function trocaLugar(array, de, para){
+    let elem1 = array[de];
+    let elem2 = array[para];
+
+    array[de] = elem2;
+    array[para] = elem1;
+}
+
+function divideNoPivo(array){
+    const pivo = array[Math.floor(array.length/2)];
+    encontraMenores(pivo, array);
+    let valoresMenores=0;
+
+    for (let analisando = 0; analisando < array.length; analisando++) {
+        let atual = array[analisando];
+        if(atual.preco < pivo.preco){
+            trocaLugar(array, analisando, valoresMenores);
+            valoresMenores++;
+        }
+    }
+
+    return array;
+}
+
+console.log(divideNoPivo(array));
